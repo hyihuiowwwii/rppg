777c48ed9a3c505084b70f8e7052224809e3dcdb
Hyihuiowwwii <87595941+Hyihuiowwwii@users.noreply.github.com>
2026-03-27 18:49:17 +0530

    Create predict_dl.py

---
+++ model/predict_dl.py
@@ -0,0 +1,16 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+
+model = load_model("bpm_model.h5")
+
+def predict_bpm_from_signal(signal_window):
+    signal_window = np.array(signal_window, dtype=np.float32)
+
+    if len(signal_window) < 150:
+        return 0
+
+    signal_window = signal_window[-150:]
+    signal_window = signal_window.reshape(1, 150, 1)
+
+    prediction = model.predict(signal_window, verbose=0)
+    return float(prediction[0][0])
